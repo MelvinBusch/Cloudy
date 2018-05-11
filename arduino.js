dcce253ffdfa0886024d88a74301ec9ee7887dbc
MelvinBusch <melvin.busch@t-online.de>
2018-05-11 23:53:08 +0200

    Ausbau des Front Ends

--- arduino.js
+++ arduino.js
@@ -1,14 +1,14 @@
-const five = require("johnny-five");
+let five = require("johnny-five");
 let board = new five.Board();
 
 board.on("ready", function() {
 
-  const button = new five.Button(2, {
+  let button = new five.Button(2, {
     "holdtime": 50
   });
 
   button.on("press", function() {
-    console.log("Hallo Welt!");
+    console.log("Hallo Papa!");
   });
 
   console.log("Board is ready!");
--- public/main.js
+++ public/main.js
@@ -1,18 +1,19 @@
 let socket = io.connect("http://localhost:3000");
-let counterSpan;
-let _counter = 0;
+let healine;
 
 socket.on("pressed", counter);
 
-document.addEventListener("DOMContentLoaded", init);
+window.addEventListener("load", init);
 
 function init() {
-  counterSpan = document.getElementById("counter");
-  console.log(counterSpan);
+  console.log("Ready to play!");
+
+  headline = document.querySelector("#game-start h1");
+  // setTimeout(5000, bounce);
 }
 
-function counter() {
-  console.log();
-  _counter++;
-  counterSpan.innerText = _counter;
+function bounce() {
+  console.log("Hallo");
 }
+
+function counter() {}
